93f4e42f074127a7027a8ef448089e37156d323d
Leomi95 <leomills2004@gmail.com>
2023-08-02 20:10:59 -0300

    hola 1

--- hola.php
+++ hola.php
@@ -1,2 +1,31 @@
-Hola
-chau
+<?php
+// Ejemplo usando MySQLi
+
+function MOSTRARDATOS($resultados){
+if ($resultados != NULL) {
+echo "-nombre ".$resultados['nombre']."<br/>";
+echo "-apellido ".$resultados['apellido']."<br/>";
+echo "-telefono ".$resultados['telefono']."<br/>";
+echo "-edad ".$resultados['edad']."<br/>";
+echo "-altura ".$resultados['altura']."<br/>";
+echo "-peso ".$resultados['peso']."<br/>";
+} else {
+echo "<br/>no hay mas datos<br/>";
+}
+}
+
+$link = mysqli_connect("localhost", "TAREAS", "");
+mysqli_select_db($link, "TAREAS");
+mysqli_query($link, "SET NAMES UTF8");
+
+$resultados = mysqli_query($link, "SELECT * FROM agenda");
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+MOSTRARDATOS(mysqli_fetch_array($resultados));
+
+mysqli_free_result($resultados);
+mysqli_close($link);
+?>
