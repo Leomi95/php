a71b1130cadf4504ce0dba07b482fae2ada51c9a
Leomi95 <leomills2004@gmail.com>
2023-08-16 20:57:24 -0300

    3

--- ejem1.php
+++ ejem1.php
@@ -1,3 +1,8 @@
-<?php
- $conex = mysqli_connect("mysql://root:Q02dQFHcfFnnP7VprRur@containers-us-west-121.railway.app:5515/railway","root","Q02dQFHcfFnnP7VprRur","railway") or die("error");
-?>
+<html>
+ <head>
+ <title> Incrustar PHP en un archivo .html </title>
+ </head>
+ <body>
+ <h1><?php echo "Hola Mun" ?></h1>
+ </body>
+ </html>
