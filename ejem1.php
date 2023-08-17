66017580a60a2eb074dcb834b93cc5cb0d3b4b08
Leomi95 <leomills2004@gmail.com>
2023-08-16 21:01:37 -0300

    lolll

--- ejem1.php
+++ ejem1.php
@@ -1,3 +1,6 @@
 <?php
  $conex = mysqli_connect("mysql://root:Q02dQFHcfFnnP7VprRur@containers-us-west-121.railway.app:5515/railway","root","Q02dQFHcfFnnP7VprRur","railway") or die("error");
+ if($conex){
+    echo "conexion exitosa";
+ }
 ?>
